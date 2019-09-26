bb13b25a1347e705acf381db0987405c482fc9d3
William Lew <WilliamMilesLew@gmail.com>
2019-09-26 11:13:15 -0700

    removed: kernel function definition

--- src/array_sum.cu
+++ src/array_sum.cu
@@ -1,13 +1,6 @@
 //input: integer
 //output: sum of all numbers in [0, integer]
 
-#include <stdio.h>
-
-__global__
-void reduceNeighboredLess(int* g_i, int* g_o, int count) {
-
-}
-
 int main(void) {
 
 }
