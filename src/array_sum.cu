f061fead11b978ffbdaed55ed7e9bd657ca6e62e
WilliamLewww <WilliamMilesLew@gmail.com>
2019-09-28 09:45:20 -0700

    cleaned: calling kernel from a separate function

--- src/array_sum.cu
+++ src/array_sum.cu
@@ -22,24 +22,42 @@ void arrayPartialSum(int* partial, int* array, int count) {
 	}
 }
 
-int main(void) {
-	int input = 1 << 9;
-	dim3 block = (32);
-	dim3 grid = ((input + block.x - 1) / block.x);
+__global__
+void arrayPartialSumUnrolled2(int* partial, int* array, int count) {
+	if (blockIdx.x * blockDim.x + threadIdx.x >= count) return;
 
-	int* host_array = (int*)malloc(input*sizeof(int));
-	for (int x = 0; x < input; x++) { host_array[x] = x; }
+	int* local_array = array + (blockIdx.x * blockDim.x);
+
+	for (int stride = blockDim.x / 2; stride > 0; stride >>= 1) {
+		if (threadIdx.x < stride) {
+			local_array[threadIdx.x] += local_array[threadIdx.x + stride];
+		}
+
+		__syncthreads();
+	}
+
+	if (threadIdx.x == 0) {
+		partial[blockIdx.x] = local_array[0];
+	}
+}
+
+int callArrayPartialSumKernel(int count) {
+	dim3 block = (64);
+	dim3 grid = ((count + block.x - 1) / block.x);
+
+	int* host_array = (int*)malloc(count*sizeof(int));
+	for (int x = 0; x < count; x++) { host_array[x] = x; }
 
 	int* device_array;
-	cudaMalloc((int**)&device_array, input*sizeof(int));
-	cudaMemcpy(device_array, host_array, input*sizeof(int), cudaMemcpyHostToDevice);
+	cudaMalloc((int**)&device_array, count*sizeof(int));
+	cudaMemcpy(device_array, host_array, count*sizeof(int), cudaMemcpyHostToDevice);
 
 	int* host_partial = (int*)malloc(grid.x*sizeof(int));
 
 	int* device_partial;
 	cudaMalloc((int**)&device_partial, grid.x*sizeof(int));
 
-	arrayPartialSum<<<grid,block>>>(device_partial, device_array, input);
+	arrayPartialSum<<<grid,block>>>(device_partial, device_array, count);
 	cudaDeviceSynchronize();
 
 	cudaMemcpy(host_partial, device_partial, grid.x*sizeof(int), cudaMemcpyDeviceToHost);
@@ -53,8 +71,14 @@ int main(void) {
 	free(host_array);
 	free(host_partial);
 
-	printf("\ntotal sum in [0, %d): %d\n\n", input, sum);
-
 	cudaDeviceReset();
+
+	return sum;
+}
+
+int main(void) {
+	int input = 1 << 16;
+	printf("\ntotal sum in [0, %d): %d\n\n", input, callArrayPartialSumKernel(input));
+	
 	return 0;
 }
