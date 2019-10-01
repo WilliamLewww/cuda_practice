86a09a1b818a8baf7829516de3a049d9c2c19232
WilliamLewww <WilliamMilesLew@gmail.com>
2019-09-30 19:40:57 -0700

    added: no sync kernel

--- src/array_sum_nested.cu
+++ src/array_sum_nested.cu
@@ -39,6 +39,24 @@ void arrayPartialSumNested(int* partial, int* array, int count) {
 	__syncthreads();
 }
 
+__global__
+void arrayPartialSumNestedNoSync(int* partial, int* array, int count) {
+	int* local_array = array + (blockDim.x * blockIdx.x);
+	int* local_partial = &partial[blockIdx.x];
+
+	if (count == 2 && threadIdx.x == 0) {
+		local_partial[0] = local_array[0] + local_array[1];
+	}
+
+	int stride = count >> 1;
+	if (stride > 1 && threadIdx.x < stride) {
+		local_array[threadIdx.x] += local_array[threadIdx.x + stride];
+		if (threadIdx.x == 0) {
+			arrayPartialSumNested<<<1, stride>>>(local_partial, local_array, stride);
+		}
+	}
+}
+
 void callNestedHelloWord() {
 	int block = 8;
 	int grid = 1;
@@ -79,6 +97,37 @@ int callArrayPartialSumNestedKernel(int* host_array, int count) {
 	return sum;
 }
 
+int callArrayPartialSumNestedNoSyncKernel(int* host_array, int count) {
+	dim3 block = (64);
+	dim3 grid = ((count + block.x - 1) / block.x);
+
+	int* device_array;
+	cudaMalloc((int**)&device_array, count*sizeof(int));
+	cudaMemcpy(device_array, host_array, count*sizeof(int), cudaMemcpyHostToDevice);
+
+	int* host_partial = (int*)malloc(grid.x*sizeof(int));
+
+	int* device_partial;
+	cudaMalloc((int**)&device_partial, grid.x*sizeof(int));
+
+	Analysis::begin();
+	arrayPartialSumNestedNoSync<<<grid,block>>>(device_partial, device_array, block.x);
+	cudaDeviceSynchronize();
+	Analysis::end(1);
+
+	cudaMemcpy(host_partial, device_partial, grid.x*sizeof(int), cudaMemcpyDeviceToHost);
+	cudaFree(device_array);
+	cudaFree(device_partial);
+
+	int sum = 0;
+	for (int x = 0; x < grid.x; x++) { sum += host_partial[x]; }
+	free(host_partial);
+
+	cudaDeviceReset();
+
+	return sum;
+}
+
 int main(void) {
 	int input = 1 << 16;
 
@@ -87,10 +136,12 @@ int main(void) {
 
 	Analysis::setAbsoluteStart();
 	Analysis::createLabel(0, "arrayPartialSumNested");
+	Analysis::createLabel(1, "arrayPartialSumNestedNoSync");
 
 	cudaDeviceReset();
 
 	printf("\n%-60s %d\n", "arrayPartialSumNested:", callArrayPartialSumNestedKernel(host_array, input));
+	printf("%-60s %d\n", "arrayPartialSumNestedNoSync:", callArrayPartialSumNestedNoSyncKernel(host_array, input));
 
 	Analysis::printAll();
 
