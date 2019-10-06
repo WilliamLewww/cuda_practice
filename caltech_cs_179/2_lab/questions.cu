6ae6291f4fe744c2a6840aebdfeb0068f225bd45
WilliamLewww <WilliamMilesLew@gmail.com>
2019-10-05 21:45:20 -0700

    finished: question 1 for lab_2

---
+++ caltech_cs_179/2_lab/questions.cu
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void question1() {
+	/*
+		latency of arithmetic instruction = ~10+ ns
+		GK110 SM (Kepler) has:
+			2 warp schedulers
+			4 warp dispatchers
+		
+		2 * 4 * 10 = 80 arithmetic instructions required
+	*/
+}
+
+void question2() {
+	/*
+	
+	*/
+}
+
+int main(void) {
+
+}
