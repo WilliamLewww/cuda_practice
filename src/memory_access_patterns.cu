7eff502f9cdb73ffb958039b0952fffaf81b8c11
William Lew <WilliamMilesLew@gmail.com>
2019-10-07 15:35:15 -0700

    added: AoS vs SoA

--- src/memory_access_patterns.cu
+++ src/memory_access_patterns.cu
@@ -1,18 +1,54 @@
 #include <stdio.h>
 
-void misalignedAccessHost(float* arrayA, float* arrayB, float* arrayC, int count, int offset) {
+struct AoS {
+	float x;
+	float y;
+};
+
+struct SoA {
+	float* x;
+	float* y;
+};
+
+void misalignedReadHost(float* arrayA, float* arrayB, float* arrayC, int count, int offset) {
 	for (int offsetIdx = offset, idx = 0; offsetIdx < count; offsetIdx++, idx++) {
 		arrayC[idx] = arrayA[offsetIdx] + arrayB[offsetIdx];
 	}
 }
 
+void misalignedWriteHost(float* arrayA, float* arrayB, float* arrayC, int count, int offset) {
+	for (int offsetIdx = offset, idx = 0; offsetIdx < count; offsetIdx++, idx++) {
+		arrayC[offsetIdx] = arrayA[idx] + arrayB[idx];
+	}
+}
+
 __global__
-void misalignedAccessDevice(float* arrayA, float* arrayB, float* arrayC, int count, int offset) {
+void arrayOfStructuresTest(AoS* arrayA, AoS* arrayB, int count) {
+
+}
+
+__global__
+void structureOfArraysTest(SoA* structureA, SoA* structureB, int count) {
+
+}
+
+__global__
+void misalignedReadDevice(float* arrayA, float* arrayB, float* arrayC, int count, int offset) {
 	int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
 	int offsetIdx = idx + offset;
 
 	if (offsetIdx < count) {
 		arrayC[idx] = arrayA[offsetIdx] + arrayB[offsetIdx];
+	}
+}
+
+__global__
+void misalignedWriteDevice(float* arrayA, float* arrayB, float* arrayC, int count, int offset) {
+	int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+	int offsetIdx = idx + offset;
+
+	if (offsetIdx < count) {
+		arrayC[offsetIdx] = arrayA[idx] + arrayB[idx];
 	}
 }
 
@@ -23,7 +59,7 @@ int main(void) {
 	dim3 block = (512);
 	dim3 grid = ((count + block.x - 1) / block.x);
 
-	int offset = 5;
+	int offset = 11;
 
 	float* h_a = (float*)malloc(count*sizeof(float));
 	float* h_b = (float*)malloc(count*sizeof(float));
@@ -36,7 +72,7 @@ int main(void) {
 	float* h_result = (float*)malloc(count*sizeof(float));
 	float* d_result = (float*)malloc(count*sizeof(float));
 
-	misalignedAccessHost(h_a, h_b, h_result, count, offset);
+	misalignedReadHost(h_a, h_b, h_result, count, offset);
 
 	float *d_a, *d_b, *d_c;
 	cudaMalloc((float**)&d_a, count*sizeof(float));
@@ -46,7 +82,7 @@ int main(void) {
 
 	cudaMalloc((float**)&d_c, count*sizeof(float));
 
-	misalignedAccessDevice<<<grid,block>>>(d_a, d_b, d_c, count, offset);
+	misalignedReadDevice<<<grid,block>>>(d_a, d_b, d_c, count, offset);
 	cudaDeviceSynchronize();
 
 	cudaMemcpy(d_result, d_c, count*sizeof(float), cudaMemcpyDeviceToHost);
