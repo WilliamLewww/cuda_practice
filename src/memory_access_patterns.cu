9f5951f63af9229c0e87db985a658e5924ab57bd
WilliamLewww <WilliamMilesLew@gmail.com>
2019-10-06 22:33:16 -0700

    added: misaligned access test

--- src/memory_access_patterns.cu
+++ src/memory_access_patterns.cu
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+void misalignedAccessHost(float* arrayA, float* arrayB, float* arrayC, int count, int offset) {
+	for (int offsetIdx = offset, idx = 0; offsetIdx < count; offsetIdx++, idx++) {
+		arrayC[idx] = arrayA[offsetIdx] + arrayB[offsetIdx];
+	}
+}
+
+__global__
+void misalignedAccessDevice(float* arrayA, float* arrayB, float* arrayC, int count, int offset) {
+	int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+	int offsetIdx = idx + offset;
+
+	if (offsetIdx < count) {
+		arrayC[idx] = arrayA[offsetIdx] + arrayB[offsetIdx];
+	}
+}
+
 int main(void) {
 	return 0;
 }
