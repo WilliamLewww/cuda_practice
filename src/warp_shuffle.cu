b2429384925db687bb9d8e95ef6342d65c09bb89
William Lew <WilliamMilesLew@gmail.com>
2019-10-16 15:20:14 -0700

    added: output

--- src/warp_shuffle.cu
+++ src/warp_shuffle.cu
@@ -6,14 +6,27 @@ void shuffleUp(float* out, float* in, int count) {
 	if (idx > count) return;
 
 	float local_value = in[idx];
-	local_value = __shfl_up_sync(0xffffffff, local_value, 1);
+	local_value = __shfl_up_sync(0xffffffff, local_value, 2);
+
+	out[idx] = local_value;
+}
+
+__global__
+void shuffleDown(float* out, float* in, int count) {
+	int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+	if (idx > count) return;
+
+	float local_value = in[idx];
+	local_value = __shfl_down_sync(0xffffffff, local_value, 2);
 
 	out[idx] = local_value;
 }
 
 int main(void) {
+	printf("\n");
+
 	int count = 32;
-	dim3 block(32);
+	dim3 block(8);
 	dim3 grid((count + block.x - 1) / block.x);
 
 	float* h_array = (float*)malloc(count*sizeof(float));
@@ -28,11 +41,21 @@ int main(void) {
 	shuffleUp<<<grid, block>>>(d_result_array, d_array, count);
 	cudaDeviceSynchronize();
 	cudaMemcpy(h_result_array, d_result_array, count*sizeof(float), cudaMemcpyDeviceToHost);
+	for (int x = 0; x < count; x++) { printf("%d ", int(h_result_array[x])); }
+	printf("\n");
+
+	shuffleDown<<<grid, block>>>(d_result_array, d_array, count);
+	cudaDeviceSynchronize();
+	cudaMemcpy(h_result_array, d_result_array, count*sizeof(float), cudaMemcpyDeviceToHost);
+	for (int x = 0; x < count; x++) { printf("%d ", int(h_result_array[x])); }
+	printf("\n");
 
 	free(h_array);
 	free(h_result_array);
 	cudaFree(d_array);
 	cudaFree(d_result_array);
 	cudaDeviceReset();
+
+	printf("\n");
 	return 0;
 }
