1da4bbcfa33b44f5abf73018b9e1455ee72d1a68
William Lew <WilliamMilesLew@gmail.com>
2019-10-23 14:17:15 -0700

    fixed: added warm up function

--- src/streams_events.cu
+++ src/streams_events.cu
@@ -9,6 +9,14 @@ void createIncrementingArray(float* out, int count) {
 	}
 }
 
+void warmUp(int count, dim3 block, dim3 grid) {
+	float* result;
+	cudaMalloc((float**)&result, count*sizeof(float));
+
+	createIncrementingArray<<<grid, block>>>(result, count);
+	cudaFree(result);
+}
+
 void createArraysNullStream(int count, dim3 block, dim3 grid) {
 	cudaEvent_t start, stop;
 	cudaEventCreate(&start);
@@ -35,10 +43,42 @@ void createArraysNullStream(int count, dim3 block, dim3 grid) {
 
 	cudaEventDestroy(start);
 	cudaEventDestroy(stop);
+
+	cudaFree(d_result_first);
+	cudaFree(d_result_second);
+	cudaFree(d_result_third);
 }
 
 void createArrayNonNullStream(int count, dim3 block, dim3 grid) {
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
 
+	cudaEventRecord(start);
+
+	float *d_result_first, *d_result_second, *d_result_third;
+	cudaMalloc((float**)&d_result_first, count*sizeof(float));
+	cudaMalloc((float**)&d_result_second, count*sizeof(float));
+	cudaMalloc((float**)&d_result_third, count*sizeof(float));
+
+	createIncrementingArray<<<grid, block>>>(d_result_first, count);
+	createIncrementingArray<<<grid, block>>>(d_result_second, count);
+	createIncrementingArray<<<grid, block>>>(d_result_third, count);
+
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+
+	float time;
+	cudaEventElapsedTime(&time, start, stop);
+
+	printf("%s%f\n", "Null-Stream: ", time);
+
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
+
+	cudaFree(d_result_first);
+	cudaFree(d_result_second);
+	cudaFree(d_result_third);
 }
 
 int main(void) {
@@ -47,9 +87,10 @@ int main(void) {
 	dim3 block(32);
 	dim3 grid((count + block.x - 1) / block.x);
 
+	warmUp(count, block, grid);
 	createArraysNullStream(count, block, grid);
 	createArrayNonNullStream(count, block, grid);
 	cudaDeviceReset();
-	
+
 	printf("\n");
 }
