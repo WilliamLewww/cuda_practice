e8a9465866d783aefbfba6abcdd04d35c41226ed
William Lew <WilliamMilesLew@gmail.com>
2019-10-24 09:32:36 -0700

    cleared: streams_events.cu functions

--- src/streams_events.cu
+++ src/streams_events.cu
@@ -1,106 +1,30 @@
 #include <stdio.h>
-#define STREAM_COUNT 3
 
 __global__
-void createIncrementingArray(float* out, int count) {
+void createIncrementingArray() {
 	int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
-
-	if (idx < count) {
-		out[idx] = idx;
-	}
 }
 
-void warmUp(int count, dim3 block, dim3 grid) {
-	float* result;
-	cudaMalloc((float**)&result, count*sizeof(float));
+void warmUp(dim3 block, dim3 grid) {
 
-	createIncrementingArray<<<grid, block>>>(result, count);
-	cudaFree(result);
 }
 
-void createArraysNullStream(int count, dim3 block, dim3 grid) {
-	cudaEvent_t start, stop;
-	cudaEventCreate(&start);
-	cudaEventCreate(&stop);
+void createArraysNullStream(dim3 block, dim3 grid) {
 
-	cudaEventRecord(start);
-
-	float *d_result_first, *d_result_second, *d_result_third;
-	cudaMalloc((float**)&d_result_first, count*sizeof(float));
-	cudaMalloc((float**)&d_result_second, count*sizeof(float));
-	cudaMalloc((float**)&d_result_third, count*sizeof(float));
-
-	for (int x = 0; x < STREAM_COUNT; x++) {
-		createIncrementingArray<<<grid, block>>>(d_result_first, count);
-		createIncrementingArray<<<grid, block>>>(d_result_second, count);
-		createIncrementingArray<<<grid, block>>>(d_result_third, count);
-	}
-
-	cudaEventRecord(stop);
-	cudaEventSynchronize(stop);
-
-	float time;
-	cudaEventElapsedTime(&time, start, stop);
-
-	printf("%-30s%f\n", "Null Stream: ", time);
-
-	cudaEventDestroy(start);
-	cudaEventDestroy(stop);
-
-	cudaFree(d_result_first);
-	cudaFree(d_result_second);
-	cudaFree(d_result_third);
 }
 
-void createArrayNonNullStream(int count, dim3 block, dim3 grid) {
-	cudaEvent_t start, stop;
-	cudaEventCreate(&start);
-	cudaEventCreate(&stop);
+void createArrayNonNullStream(dim3 block, dim3 grid) {
 
-	cudaEventRecord(start);
-
-	float *d_result_first, *d_result_second, *d_result_third;
-	cudaMalloc((float**)&d_result_first, count*sizeof(float));
-	cudaMalloc((float**)&d_result_second, count*sizeof(float));
-	cudaMalloc((float**)&d_result_third, count*sizeof(float));
-
-	int streamCount = STREAM_COUNT;
-	cudaStream_t* streams = (cudaStream_t*)malloc(streamCount*sizeof(cudaStream_t));
-	for (int x = 0; x < streamCount; x++) {
-		cudaStreamCreate(&streams[x]);
-	}
-
-	for (int x = 0; x < streamCount; x++) {
-		createIncrementingArray<<<grid, block, 0, streams[x]>>>(d_result_first, count);
-		createIncrementingArray<<<grid, block, 0, streams[x]>>>(d_result_first, count);
-		createIncrementingArray<<<grid, block, 0, streams[x]>>>(d_result_first, count);
-	}
-
-	cudaEventRecord(stop);
-	cudaEventSynchronize(stop);
-
-	float time;
-	cudaEventElapsedTime(&time, start, stop);
-
-	printf("%-30s%f\n", "Non-Null Stream: ", time);
-
-	cudaEventDestroy(start);
-	cudaEventDestroy(stop);
-
-	cudaFree(d_result_first);
-	cudaFree(d_result_second);
-	cudaFree(d_result_third);
 }
 
 int main(void) {
 	printf("\n");
-	int count = 1 << 16;
-	dim3 block(32);
-	dim3 grid((count + block.x - 1) / block.x);
+	dim3 block(1);
+	dim3 grid(1);
 
-	warmUp(count, block, grid);
-	createArraysNullStream(count, block, grid);
-	createArrayNonNullStream(count, block, grid);
+	warmUp(block, grid);
+	createArraysNullStream(block, grid);
+	createArrayNonNullStream(block, grid);
 	cudaDeviceReset();
 
 	printf("\n");
